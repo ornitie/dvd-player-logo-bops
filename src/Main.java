36504e0be459a75bb07887680a755671069dd395
Juan Pablo <uncorreodeinternet@gmail.com>
2021-06-17 17:03:51 -0500

    [#6] Added Logo Entity

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,6 @@ import java.awt.*;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
         Frame frame = new Frame();
         Logo label = new Logo();
         frame.frame.add(label);
