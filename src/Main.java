0eccaeead42ec8f69cdd71b959f9ce7c0d9ba101
Juan Pablo <uncorreodeinternet@gmail.com>
2021-06-19 17:03:51 -0500

    [#8] Base Movement

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.awt.*;
+import java.util.concurrent.TimeUnit;
 
 public class Main {
     public static void main(String[] args) {
@@ -7,5 +8,16 @@ public class Main {
         frame.frame.add(label);
         frame.frame.getContentPane().setBackground(Color.BLACK);
         frame.frame.repaint();
+        frame.frame.setLayout(null);
+        label.setLocation(0, 0);
+        frame.frame.repaint();
+        for (int i = 0; i < 200; i++) {
+            try {
+                label.setLocation(i, i);
+                TimeUnit.MILLISECONDS.sleep(17);
+            } catch (Exception e) {
+
+            }
+        }
     }
 }
